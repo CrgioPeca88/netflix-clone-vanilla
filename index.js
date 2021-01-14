a8e1be838d40afad3704425c0d8faa40524ae751
CrgioPeca88 <crgiopeca88@gmail.com>
2021-01-14 08:18:08 -0500

    [FEATURE] live-server added - npm module added into package.json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import VideoPlayer from './src/modules/shared/video-player.js';
+
 (function(){
+  const test = new VideoPlayer();
   const video = document.getElementById('featured-video');
   const ppButton = document.getElementById('play-button');
 
--- src/modules/shared/video-player.js
+++ src/modules/shared/video-player.js
@@ -1,1 +1,7 @@
 'use strict';
+
+function VideoPlayer() {
+  console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+}
+
+export default VideoPlayer;
