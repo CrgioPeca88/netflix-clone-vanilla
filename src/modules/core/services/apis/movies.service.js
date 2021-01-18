ff13ad297e148011871a456ed94026e9a756ce0d
CrgioPeca88 <crgiopeca88@gmail.com>
2021-01-18 16:44:18 -0500

    [FEATURE] links for detail page added - render.service adjusted - movies.service adjusted

--- src/modules/core/services/apis/movies.service.js
+++ src/modules/core/services/apis/movies.service.js
@@ -2,26 +2,32 @@
 
 function MoviesService() {
   this.movies = [{
+    id: 1,
     imgSrc: './assets/images/covers/matrix.png',
     imgAlt: 'Popular movie matrix',
     imgClass: 'img-cover'
   }, {
+    id: 2,
     imgSrc: './assets/images/covers/bob.png',
     imgAlt: 'Popular movie bob',
     imgClass: 'img-cover'
   }, {
+    id: 3,
     imgSrc: './assets/images/covers/spider-man.png',
     imgAlt: 'Popular movie spider',
     imgClass: 'img-cover'
   }, {
+    id: 4,
     imgSrc: './assets/images/covers/dragon.png',
     imgAlt: 'Popular movie dragon',
     imgClass: 'img-cover'
   }, {
+    id: 5,
     imgSrc: './assets/images/covers/john.png',
     imgAlt: 'Popular movie john',
     imgClass: 'img-cover'
   }, {
+    id: 6,
     imgSrc: './assets/images/covers/santa.png',
     imgAlt: 'Popular movie santa',
     imgClass: 'img-cover'
--- src/modules/core/services/render.service.js
+++ src/modules/core/services/render.service.js
@@ -5,7 +5,7 @@ function RenderService() { }
 RenderService.prototype.renderMoviesCover = function(containerElement, movies) {
   let moviesHtml = '';
   movies.forEach((movie) => {
-    moviesHtml = moviesHtml + `<a href="#"><img src="${movie.imgSrc}" alt="${movie.imgAlt}" class="${movie.imgClass}"></a>`;
+    moviesHtml = moviesHtml + `<a href="/src/modules/detail/index.html?movie=${movie.id}"><img src="${movie.imgSrc}" alt="${movie.imgAlt}" class="${movie.imgClass}"></a>`;
   });
   containerElement.innerHTML = moviesHtml;
 }
