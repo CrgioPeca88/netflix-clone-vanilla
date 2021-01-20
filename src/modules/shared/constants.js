3d7c2e375846a9eb30535912b1ff0b53dab714c0
CrgioPeca88 <crgiopeca88@gmail.com>
2021-01-20 17:39:26 -0500

    [FEATURE] Github endpoint constant adjusted;

--- src/modules/shared/constants.js
+++ src/modules/shared/constants.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function Constants() {
-  this.localEnvironment = 'http://localhost:8080'
-  //this.localEnvironment = 'https://crgiopeca88.github.io/netflix-clone-vanilla'
+  //this.localEnvironment = 'http://localhost:8080'
+  this.localEnvironment = 'https://crgiopeca88.github.io/netflix-clone-vanilla'
 
   const getLocalEnvironment = () => { return this.localEnvironment; };
 
